bce21ad8b71a784e4e3d0659048045eaa6b45c28
xrey007 <reinaldo.leal001@gmail.com>
2016-11-10 10:06:46 -0500

    adding edits and fixing loop logic for the comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,5 +8,20 @@ class PostsController < ApplicationController
   def create
     @post = Post.new
   end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
 
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
