62a9b6b0017d3e12c48c81595e05315ac5ae6d91
xrey007 <reinaldo.leal001@gmail.com>
2016-11-09 19:56:23 -0500

    adding blog app

---
+++ app/controllers/comment_controller.rb
@@ -0,0 +1,12 @@
+class CommentController < ApplicationController
+
+  def index
+    @comments = Comment.all
+  end
+def new
+  @comment = Comment.new
+end
+  def show
+    @comment = Comment.find(params[:id])
+  end
+end
